f236a6edb9a576f66a1358043a03b87885da3f42
JamshidXolmominov <xolmominovj2023@gmail.com>
2023-05-15 13:38:13 +0300

    Filter all instructors & Approve and delete instructor

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -2,31 +2,62 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Instructor, InstructorDocument } from 'src/instructor/instructor.model';
+import { User, UserDocument } from 'src/user/user.model';
 
 @Injectable()
 export class AdminService {
-  constructor(@InjectModel(Instructor.name) private instructorModel: Model<InstructorDocument>) {}
+  constructor(
+    @InjectModel(Instructor.name) private instructorModel: Model<InstructorDocument>,
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+  ) {}
 
   async getAllInstructors() {
-    const approvedInstructor = await this.instructorModel
-      .find({ approved: true })
-      .populate('author')
-      .exec();
-    const appliedInstructor = await this.instructorModel
-      .find({ approved: false })
-      .populate('author')
-      .exec();
+    const instructors = await this.instructorModel.find().populate('author').exec();
 
-    return {
-      approved: approvedInstructor.map(instructor => this.getSpecificField(instructor)),
-      applied: appliedInstructor.map(instructor => this.getSpecificField(instructor)),
-    };
+    return instructors.map(instructor => this.getSpecificField(instructor));
+  }
+
+  async aproveInstructor(instructorId: string) {
+    const instructor = await this.instructorModel.findByIdAndUpdate(
+      instructorId,
+      {
+        $set: { approved: true },
+      },
+      { new: true },
+    );
+
+    await this.userModel.findByIdAndUpdate(
+      instructor.author,
+      { $set: { role: 'INSTRUCTOR' } },
+      { new: true },
+    );
+
+    return 'Success';
+  }
+
+  async deleteIntructor(instructorId: string) {
+    const instructor = await this.instructorModel.findByIdAndUpdate(
+      instructorId,
+      {
+        $set: { approved: false },
+      },
+      { new: true },
+    );
+
+    await this.userModel.findByIdAndUpdate(
+      instructor.author,
+      { $set: { role: 'USER' } },
+      { new: true },
+    );
+
+    return 'Success';
   }
 
   getSpecificField(instructor: InstructorDocument) {
     return {
       approved: instructor.approved,
       socialMedia: instructor.socialMedia,
+      _id: instructor._id,
       author: {
         fullName: instructor.author.fullName,
         email: instructor.author.email,
